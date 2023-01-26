5d165ce6e34b5df7cc3c54032fa6748d1bdb25b7
Davidsonity <eshemitanvoke@gmail>
2023-01-26 11:19:53 +0100

    update

--- app.py
+++ app.py
@@ -18,55 +18,55 @@ team_df = pd.DataFrame.from_dict(team_dict)
 teams = team_df['Team'].tolist()
 
 
-class Match():
-    '''
-    Match Webscraping and Analysis.
-    Scraping Matches and
-
-    Parameters
-    ----------
-    agent : string, default= Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36
-        a characteristic string that lets servers and network peers identify the application,
-        operating system, vendor, and/or version of the requesting user agent.
-
-    '''
-
-    def __init__(self,
-                 agent='Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'):
-        self.agent = agent
-
-    def last_fixtures(self, team, league):
-        '''
-        Obtain last 5 fixtures
-
-        Parameters
-        ----------
-        team : string,
-            name of team
-
-        league : string,
-            name of league
-
-        Returns
-        -------
-        C : dict
-            Returns last 5 fixtures
-
-        '''
-        url = team_df[(team_df['Team'] == team) & (team_df['League'] == league)]['url'].values[0]
-        response = requests.get(url, headers={'User-Agent': self.agent})
-
-        try:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            table = soup.find_all('table', class_='matches')
-            df = pd.read_html(str(table))[0]
-            df.drop(df.columns[-2:], axis=1, inplace=True)
-            df.rename(columns={'Outcome': 'Home team', 'Home team': 'Outcome', 'Score/Time': 'Away team',
-                               'Competition': 'League'}, inplace=True)
-
-            return df[:5], df[5:]
-        except ImportError:
-            return ('incorect teamname, country or both')
+# class Match():
+#     '''
+#     Match Webscraping and Analysis.
+#     Scraping Matches and
+#
+#     Parameters
+#     ----------
+#     agent : string, default= Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36
+#         a characteristic string that lets servers and network peers identify the application,
+#         operating system, vendor, and/or version of the requesting user agent.
+#
+#     '''
+#
+#     def __init__(self,
+#                  agent='Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'):
+#         self.agent = agent
+#
+#     def last_fixtures(self, team, league):
+#         '''
+#         Obtain last 5 fixtures
+#
+#         Parameters
+#         ----------
+#         team : string,
+#             name of team
+#
+#         league : string,
+#             name of league
+#
+#         Returns
+#         -------
+#         C : dict
+#             Returns last 5 fixtures
+#
+#         '''
+#         url = team_df[(team_df['Team'] == team) & (team_df['League'] == league)]['url'].values[0]
+#         response = requests.get(url, headers={'User-Agent': self.agent})
+#
+#         try:
+#             soup = BeautifulSoup(response.text, 'html.parser')
+#             table = soup.find_all('table', class_='matches')
+#             df = pd.read_html(str(table))[0]
+#             df.drop(df.columns[-2:], axis=1, inplace=True)
+#             df.rename(columns={'Outcome': 'Home team', 'Home team': 'Outcome', 'Score/Time': 'Away team',
+#                                'Competition': 'League'}, inplace=True)
+#
+#             return df[:5], df[5:]
+#         except ImportError:
+#             return ('incorect teamname, country or both')
 
 
 ####################################################################
@@ -95,17 +95,32 @@ league_ = st.selectbox(
 )
 
 if st.button('Fixtures'):
-    matches = Match()
-    last_5, next_5 = matches.last_fixtures(team_, league_)
-    # Indexing from 1
-    last_5.index = np.arange(1, len(last_5) + 1)
+    # matches = Match()
+    # last_5, next_5 = matches.last_fixtures(team_, league_)
 
-    # Rename column and start index from 1
-    next_5.rename(columns={'Outcome': 'Time'}, inplace=True)
-    next_5.index = np.arange(1, len(next_5) + 1)
+    url = team_df[(team_df['Team'] == team_) & (team_df['League'] == league_)]['url'].values[0]
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'})
 
-    st.subheader("Next 5 Fixtures")
-    st.dataframe(data=next_5)
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find_all('table', class_='matches')
+        df = pd.read_html(str(table))[0]
+        df.drop(df.columns[-2:], axis=1, inplace=True)
+        df.rename(columns={'Outcome': 'Home team', 'Home team': 'Outcome', 'Score/Time': 'Away team',
+                           'Competition': 'League'}, inplace=True)
 
-    st.subheader("Last 5 Fixtures")
-    st.dataframe(data=last_5)
+        st.dataframe(data=df)
+    except ImportError:
+        st.warnings('incorect teamname, country or both')
+    # # Indexing from 1
+    # last_5.index = np.arange(1, len(last_5) + 1)
+    #
+    # # Rename column and start index from 1
+    # next_5.rename(columns={'Outcome': 'Time'}, inplace=True)
+    # next_5.index = np.arange(1, len(next_5) + 1)
+    #
+    # st.subheader("Next 5 Fixtures")
+    # st.dataframe(data=next_5)
+    #
+    # st.subheader("Last 5 Fixtures")
+    # st.dataframe(data=last_5)
